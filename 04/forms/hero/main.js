b5d69d7af6c8d9d71c0dbbb694164daf813f8e2f
bencornia <bencornia@byui.edu>
2022-05-16 19:04:55 -0600

    Add notes for forms

--- 04/forms/hero/main.js
+++ 04/forms/hero/main.js
@@ -6,6 +6,22 @@ function makeHero(event) {
 	const hero = {};
 	hero.name = form.heroName.value;
 	hero.realName = form.realName.value;
+
+	// Get superpowers
+	hero.powers = [...form.powers].filter((box) => box.checked).map((box) => box.value);
+
+	// Get type of hero
+	hero.category = form.category.value;
+
+	// Get age
+	hero.age = form.age.value;
+
+	// Get city
+	hero.city = form.city.value;
+
+	// Get origin
+	hero.origin = form.origin.value;
+
 	alert(JSON.stringify(hero));
 	return hero;
 }
