dfc76c72622979eb6a0c3ac311a8563d59c3563a
corniab <bencornia@byui.edu>
2022-06-11 23:21:53 -0600

    Add notes for canvas.

---
+++ 08/canvas/main.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
+context.strokeStyle = "red";
+context.fillStyle = "blue";
+context.fillRect(10, 10, 100, 100);
+context.strokeRect(10, 10, 100, 100);
